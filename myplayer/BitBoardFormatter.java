1aab61584397276745c9f73da21ca6a52e5f6384
ReijiIDA <cnt14012@ict.nitech.ac.jp>
2025-06-04 16:17:30 +0900

    Add files via upload
    まだバグが残っていますが、いったんあげておきます。

---
+++ myplayer/BitBoardFormatter.java
@@ -0,0 +1,61 @@
+package myplayer;
+
+import static ap25.Board.*;
+import static ap25.Color.*;
+
+import java.util.List;
+import java.util.Map;
+
+import ap25.*;
+
+public class BitBoardFormatter {
+  public static String format(BitBoard board) {
+    var turn = board.getTurn();
+    var move = board.getMove();
+    var blacks = board.findNoPassLegalIndexes(BLACK);
+    var whites = board.findNoPassLegalIndexes(WHITE);
+    var legals = Map.of(BLACK, blacks, WHITE, whites);
+
+    var buf = new StringBuilder("  ");
+    for (int k = 0; k < SIZE; k++) buf.append(Move.toColString(k));
+    buf.append("\n");
+
+    for (int k6 = 0; k6 < SIZE * SIZE; k6++) {
+      int col = k6 % SIZE;
+      int row = k6 / SIZE;
+      int k8 = 8 * (row + 1) + col + 1;
+
+      if (col == 0) buf.append((row + 1) + "|");
+
+      if (board.get(k8) == NONE) {
+        boolean legal = false;
+        var b = blacks.contains(k8);
+        var w = whites.contains(k8);
+        if (turn == BLACK && b) legal = true;
+        if (turn == WHITE && w) legal = true;
+        buf.append(legal ? '.' : ' ');
+      } else {
+        var s = board.get(k8).toString();
+        if (move != null && k6 == move.getIndex()) s = s.toUpperCase();
+        buf.append(s);
+      }
+
+      if (col == SIZE - 1) {
+        buf.append("| ");
+        if (row == 0 && move != null) {
+          buf.append(move);
+        } else if (row == 1) {
+          buf.append(turn + ": " + toString(legals.get(turn)));
+        }
+        buf.append("\n");
+      }
+    }
+
+    buf.setLength(buf.length() - 1);
+    return buf.toString();
+  }
+
+  static List<String> toString(List<Integer> moves) {
+    return moves.stream().map(k -> Move.toIndexString(k)).toList();
+  }
+}
