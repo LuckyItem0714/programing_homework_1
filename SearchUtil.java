6f24e1d19af08947c1378cc7fc3ed7b1630894c3
ReijiIDA <cnt14012@ict.nitech.ac.jp>
2025-06-14 17:58:08 +0900

    Delete SearchUtil.java

--- SearchUtil.java
+++
@@ -1,9 +0,0 @@
-package myplayer;
-
-import ap25.*;
-
-public class SearchUtil{
-    public static boolean isTerminal(Board board, int depth) {// ゲーム終了または探索深さ制限に達したかを判定。
-		return board.isEnd() || depth < 0;
-	}
-}
